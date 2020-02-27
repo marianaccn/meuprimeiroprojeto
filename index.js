4d46af0cb19f98394fade61e66fd1839b7ac86ad
mariana cavalcante <marianacavalcante.nasc@gmail.com>
2020-02-26 22:33:30 -0300

    adicionando conexao ao banco de dados

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/primeiraApi', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(function(){
+    console.log("conectou no banco")
+}).catch(function(){
+    console.log("nao foi possivel conectar no banco")
+})
+
+const comidas = mongoose.model('comidas', { 
+    name: String,
+    categoria: String,
+});
+
 var express = require('express');
 
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('alguma coisa');
+    const food = new comidas({ 
+        name: 'Coxinha',
+        categoria: "salgado",
+    });
+    food.save().then(() => console.log('ta pronto chefia'));
+    res.send(food);
 });
 
 app.listen(3000, function () {
